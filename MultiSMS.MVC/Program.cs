f8e61f950aec603faf576ce2889eaea00160de6c
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-12-05 13:56:57 +0100

    Added migration and built the database, Injected the right type of entity into Identity.

--- MultiSMS.MVC/Program.cs
+++ MultiSMS.MVC/Program.cs
@@ -1,30 +1,33 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MultiSMS.Interface;
+using MultiSMS.Interface.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var multiSMSConnectionString = builder.Configuration.GetConnectionString("MultiSMSConnectionString") ?? throw new InvalidOperationException($"Could not get connection string for parliament election");
-builder.Services.AddDbContext<MultiSMSDbContext>(options =>
-    options.UseSqlServer(multiSMSConnectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<MultiSMSDbContext>();
-builder.Services.AddControllersWithViews();
 
 var configuration = new ConfigurationBuilder()
    .SetBasePath(builder.Environment.ContentRootPath)
    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
 .Build();
 
+builder.Services.AddOptions();
+
+var multiSMSConnectionString = builder.Configuration.GetConnectionString("MultiSMSConnectionString") ?? throw new InvalidOperationException($"Could not get connection string for parliament election");
+builder.Services.AddDbContext<MultiSMSDbContext>(options =>
+    options.UseSqlServer(multiSMSConnectionString));
+
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<MultiSMSDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
@@ -34,9 +37,12 @@ else
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
