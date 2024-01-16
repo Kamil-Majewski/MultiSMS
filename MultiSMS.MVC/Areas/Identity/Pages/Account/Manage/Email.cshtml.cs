641a01778e9839e7051f948348279ec96b479725
Kamil Majewski <kamil.majewski.t@gmail.com>
2024-01-16 09:33:54 +0100

    Updated visual layout of Email page in profile settings.

--- MultiSMS.MVC/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ MultiSMS.MVC/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -68,9 +68,9 @@ namespace MultiSMS.MVC.Areas.Identity.Pages.Account.Manage
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "New email")]
+            [Required(ErrorMessage = "Pole nowy adres email jest wymagane")]
+            [EmailAddress(ErrorMessage = "Podana wartość nie jest adresem email")]
+            [Display(Name = "Nowy adres email")]
             public string NewEmail { get; set; }
         }
 
@@ -78,11 +78,6 @@ namespace MultiSMS.MVC.Areas.Identity.Pages.Account.Manage
         {
             var email = await _userManager.GetEmailAsync(user);
             Email = email;
-
-            Input = new InputModel
-            {
-                NewEmail = email,
-            };
 
             IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
         }
@@ -129,43 +124,11 @@ namespace MultiSMS.MVC.Areas.Identity.Pages.Account.Manage
                     "Confirm your email",
                     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                StatusMessage = "Wysłano link potwierdzający zmianę adresu e-mail na twoją skrzynkę pocztową";
                 return RedirectToPage();
             }
 
-            StatusMessage = "Your email is unchanged.";
-            return RedirectToPage();
-        }
-
-        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
-        {
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                await LoadAsync(user);
-                return Page();
-            }
-
-            var userId = await _userManager.GetUserIdAsync(user);
-            var email = await _userManager.GetEmailAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { area = "Identity", userId = userId, code = code },
-                protocol: Request.Scheme);
-            await _emailSender.SendEmailAsync(
-                email,
-                "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-            StatusMessage = "Verification email sent. Please check your email.";
+            StatusMessage = "Nie wprowadzono żadnych zmian";
             return RedirectToPage();
         }
     }
