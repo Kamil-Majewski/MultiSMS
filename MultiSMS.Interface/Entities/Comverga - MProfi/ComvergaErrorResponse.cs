24604032074799bc800cb9563cf76fd3fa38f930
Kamil Majewski <kamil.majewski.t@gmail.com>
2025-02-14 14:48:55 +0100

    - Created entities for Comverga API support.

---
+++ MultiSMS.Interface/Entities/Comverga - MProfi/ComvergaErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace MultiSMS.Interface.Entities.Comverga___MProfi
+{
+    public class ComvergaErrorResponse
+    {
+        [JsonProperty(PropertyName = "detail", Required = Required.Always)]
+        public string Detail { get; set; } = default!;
+    }
+}
---
+++ MultiSMS.Interface/Entities/Comverga - MProfi/ComvergaSuccessResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MultiSMS.Interface.Entities.Comverga___MProfi
+{
+    public class ComvergaSuccessResponse
+    {
+        [JsonProperty(PropertyName = "result", Required = Required.Always)]
+        public List<ResultItem> Result { get; set; } = new();
+    }
+
+    public class ResultItem
+    {
+        [JsonProperty(PropertyName = "id", Required = Required.Always)]
+        public int Id { get; set; }
+    }
+}
